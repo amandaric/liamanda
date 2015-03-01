25d1e39e04bae35388af5c6f5a88815e74198ea4
Liam Staskawicz <liam@stask.net>
2015-03-01 12:30:22 -0800

    burger menu: better fix for collapsing when item is selected

--- source/js/all.js
+++ source/js/all.js
@@ -1,2 +1,12 @@
 //= require vendor/jquery.min
 //= require vendor/bootstrap.min
+
+
+// if hamburger menu is expanded, ensure it collapses when an item is clicked
+// https://github.com/twbs/bootstrap/issues/9013
+// https://github.com/twbs/bootstrap/issues/12852
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
